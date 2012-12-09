c01acc6fc417fb5f02a051e9833886ee53f16703
Alex Kwan <alexkwan089@gmail.com>
2012-12-08 22:22:34 -0800

    adding intial zapbox starrtup

---
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -0,0 +1,48 @@
+package bootstrap.liftweb
+
+import net.liftweb._
+import util._
+import Helpers._
+
+import common._
+import http._
+import sitemap._
+import Loc._
+
+
+/**
+ * A class that's instantiated early and run.  It allows the application
+ * to modify lift's environment
+ */
+class Boot {
+  def boot {
+    // where to search snippet
+    LiftRules.addToPackages("code")
+
+    // Build SiteMap
+    val entries = List(
+      Menu(Loc("Home", List("index"), "Home", Loc.Stateless)),
+
+      // more complex because this menu allows anything in the
+      // /static path to be visible
+      Menu(Loc("Static", Link(List("static"), true, "/static/index"), 
+         "Static Content",  Loc.Stateless)))
+
+    // set the sitemap.  Note if you don't want access control for
+    // each page, just comment this line out.
+    LiftRules.setSiteMap(SiteMap(entries:_*))
+
+    // Use jQuery 1.4
+    LiftRules.jsArtifacts = net.liftweb.http.js.jquery.JQuery14Artifacts
+
+    //Don't use state with Heroku
+    LiftRules.autoIncludeAjax = _ => false
+    LiftRules.statelessTest.append {
+      case _ => true
+    }
+
+    // Force the request to be UTF-8
+    LiftRules.early.append(_.setCharacterEncoding("UTF-8"))
+
+  }
+}
